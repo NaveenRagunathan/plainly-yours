73c2cc090590ec72fc8d7127412322c14c5c0448
Naveen R <naveenjeeva2105@gmail.com>
2026-01-11 08:34:15 +0530

    fix: remove react router basename for netlify deployment

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <ToasterSonner />
-      <BrowserRouter basename="/plainly-yours/" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
